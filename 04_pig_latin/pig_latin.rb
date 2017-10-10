30e477acd19d9b6e14ab8acdb47111bdf89e4e06
Tarek Dinkespiler <tarek.dinkespiler@gmail.com>
2017-10-10 23:05:46 +0200

    Pig Latin 3 exos

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,17 @@
 #write your code here
+def translate(message)
+a = []
+
+message.each_byte do |letter|
+	a << letter.chr
+end
+
+until a[0] == "a" || a[0] == "e"  || a[0] == "i"  || a[0] == "o"  || a[0] == "u" || a[0] == "y"    
+a << a[0]
+a = a.drop(1)
+end
+
+a.join() + "ay"
+
+end
+
