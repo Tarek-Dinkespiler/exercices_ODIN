dfb958388b4a1b512c24e16fc91304fce4c08ba6
Tarek Dinkespiler <tarek.dinkespiler@gmail.com>
2017-10-11 00:10:40 +0200

    phoneme challenge

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,17 +1,27 @@
 #write your code here
-def translate(message)
-a = []
+def translate(sentence)
 
-message.each_byte do |letter|
-	a << letter.chr
+  b = sentence.split(" ")
+  @c = []
+
+	b.each do |word|
+	 
+	  a = []
+
+	  word.each_byte do |letter|
+    	
+	    a << letter.chr
+	  end
+
+      until a[0] == "a" || a[0] == "e"  || a[0] == "i"  || a[0] == "o"  || a[0] == "u" || a[0] == "y"    
+	    a << a[0]
+	    a = a.drop(1)
+	  end
+	
+	@c << a.join() + "ay"
+    
+    end
+    
+    @c.join(" ")
+
 end
-
-until a[0] == "a" || a[0] == "e"  || a[0] == "i"  || a[0] == "o"  || a[0] == "u" || a[0] == "y"    
-a << a[0]
-a = a.drop(1)
-end
-
-a.join() + "ay"
-
-end
-
